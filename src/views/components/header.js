fbbd88f9e5c1cc040cc2cc5259f014edce650413
haru1843 <haru1843inf@gmail.com>
2021-03-22 00:54:31 +0900

    update: simple header

--- src/views/components/header.js
+++ src/views/components/header.js
@@ -22,10 +22,41 @@ const theme = createMuiTheme({
 const HeaderDiv = styled.div`
   z-index: 99999;
 
-  a {
-    color: rgb(200, 200, 200);
-    text-decoration: none;
-    font-family: "M PLUS 1p", "Helvetica Neue", Arial, sans-serif;
+  div.header-right {
+    margin-left: auto;
+    /* margin-right: -12; */
+
+    .link-div {
+      border-left: 2px solid #505050;
+      width: 150px;
+      text-align: center;
+      display: inline-block;
+      /* margin: 0; */
+
+      a {
+        color: rgb(200, 200, 200);
+        display: block;
+        width: 100%;
+        height: 100%;
+
+        text-decoration: none;
+        font-family: "M PLUS 1p", "Helvetica Neue", Arial, sans-serif;
+        transition: 0.3s ease-out;
+        text-shadow: 0px 0px 0px rgba(170, 170, 170, 0.6);
+      }
+
+      /* "to left" / "to right" - affects initial color */
+      background: linear-gradient(to left, #202020 50%, #505050 50%) right;
+      background-size: 200%;
+      transition: 0.2s ease-out;
+
+      &:hover {
+        background-position: left;
+        a {
+          text-shadow: 2px 2px 2px rgba(150, 150, 150, 0.5);
+        }
+      }
+    }
   }
 `;
 
@@ -35,30 +66,23 @@ function Header(props) {
       <ThemeProvider theme={theme}>
         <AppBar position="static">
           <Toolbar>
-            <div>
-              <Link className="header-link" to="/top">
-                トップ
-              </Link>
-            </div>
-            <div>
-              <Link className="header-link" to="/usage">
-                使い方
-              </Link>
-            </div>
-            <div>
-              <Link className="header-link" to="/exercise">
-                練習問題
-              </Link>
-            </div>
-            <div>
-              <Link className="header-link" to="/work1">
-                課題1
-              </Link>
-            </div>
-            <div>
-              <Link className="header-link" to="/work2">
-                課題2
-              </Link>
+            <span>回路シミュレータ</span>
+            <div className="header-right">
+              <div className="link-div">
+                <Link to="/top">トップ</Link>
+              </div>
+              <div className="link-div">
+                <Link to="/usage">使い方</Link>
+              </div>
+              <div className="link-div">
+                <Link to="/exercise">練習</Link>
+              </div>
+              <div className="link-div">
+                <Link to="/work1">課題1</Link>
+              </div>
+              <div className="link-div">
+                <Link to="/work2">課題2</Link>
+              </div>
             </div>
           </Toolbar>
         </AppBar>
