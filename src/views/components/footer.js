a9f4cafcee5357c1f795eefb3f9456b9f3b29d90
haru1843 <haru1843inf@gmail.com>
2021-03-28 19:06:56 +0900

    update: footer compleate

--- src/views/components/footer.js
+++ src/views/components/footer.js
@@ -3,47 +3,75 @@ import React from "react";
 
 import styled from "styled-components";
 
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: "#303030",
-      main: "#202020",
-      dark: "#101010",
-      contrastText: "#fff",
-    },
-  },
-});
-
 const FooterrDiv = styled.div`
   z-index: 99999;
 
+  display: flex;
+  align-items: center;
+
   background-color: rgb(32, 32, 32);
+  color: rgb(200, 200, 200);
+  font-size: 0.8rem;
 
   img.lab-logo {
-    width: 20%;
+    width: 10%;
+    margin: 1rem;
+  }
+
+  span {
+    display: inline-block;
+
+    /* position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    -webkit-transform: translateY(-50%); */
+
+    &.righting {
+    }
+    &.link-container {
+      margin-left: auto;
+      margin-right: 2rem;
+
+      a {
+        margin-left: 1em;
+        text-decoration: none;
+        color: rgb(200, 200, 200);
+
+        &:hover {
+          color: rgb(230, 230, 230);
+        }
+      }
+    }
   }
 `;
 
 function Footer(props) {
   return (
     <FooterrDiv>
-      <div>
-        <img
-          className="lab-logo"
-          src={`${process.env.PUBLIC_URL}/footer/lab_logo.svg`}
-          alt="デバイスの画像"
-        ></img>
-        <span>
-          <span>hoge</span>
-          <span>fuga</span>
-          <span>piyo</span>
-        </span>
-      </div>
-      <div>{"(C) 2020- nishimori lab. All Rights Reserved."}</div>
+      <img
+        className="lab-logo"
+        src={`${process.env.PUBLIC_URL}/footer/lab_logo.svg`}
+        alt="デバイスの画像"
+      ></img>
+      <span className="righting">
+        {"(C) 2020- nishimori lab. All Rights Reserved."}
+      </span>
+      <span className="link-container">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://haru1843.github.io/simulate_circuit/"
+        >
+          旧ページ
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://nu-nishimori-lab.netlify.app/"
+        >
+          研究室HP
+        </a>
+      </span>
     </FooterrDiv>
   );
 }
